3328eb44e19972f7ab5aebb05642ad2d6ea3f381
hengzizng <hengzizng@naver.com>
2024-09-10 09:44:55 +0900

    test

--- src/test/kotlin/lotto/LottoNumberTest.kt
+++ src/test/kotlin/lotto/LottoNumberTest.kt
@@ -1,5 +1,7 @@
 package lotto
 
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.types.shouldBeSameInstanceAs
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
@@ -7,17 +9,27 @@ import org.junit.jupiter.api.assertThrows
 class LottoNumberTest {
     @Test
     fun test1() {
-        assertThrows<IllegalArgumentException> { LottoNumber(0) }
-        assertThrows<IllegalArgumentException> { LottoNumber(46) }
+        assertThrows<IllegalArgumentException> { LottoNumber.from(0) }
+        assertThrows<IllegalArgumentException> { LottoNumber.from(46) }
     }
 
     @Test
     fun test2() {
-        assertDoesNotThrow { LottoNumber(0) }
-        assertDoesNotThrow { LottoNumber(46) }
+        assertDoesNotThrow { LottoNumber.from(0) }
+        assertDoesNotThrow { LottoNumber.from(46) }
     }
 
     @Test
     fun `동등성`() {
+        val number1 = LottoNumber.from(1)
+        val number2 = LottoNumber.from(1)
+        number1 shouldBe number2
+    }
+
+    @Test
+    fun `동일성`() {
+        val number1 = LottoNumber.from(1)
+        val number2 = LottoNumber.from(1)
+        number1 shouldBeSameInstanceAs number2
     }
 }
